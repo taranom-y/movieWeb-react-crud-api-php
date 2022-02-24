4f8061375a2d81e4f4bc10285b50fd2cedb71080
taranom-y <tarannom.yousefi98@gmail.com>
2022-02-24 17:49:16 +0330

    init

---
+++ my-app/src/movie.jsx
@@ -0,0 +1,37 @@
+import {useState,useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+
+ export default function Movie () {
+
+  const [movie, setMovies] = useState([]);
+
+  useEffect(() => {
+      getMovie();
+  }, []);
+
+    const {id} =useParams();
+
+    function getMovie() {
+        axios.get(`http://localhost:8080/workSpace/taranom.yousefi-finalProject/my-app/api/movie/${id}`).then(function(response) {
+            console.log(response.data);
+            setMovies(response.data);
+        });
+    }
+
+
+   return (
+    <div>
+         <div className="row mt-5"> 
+                    <div className='col text-center p-5 mt-5'>
+                            <p>{movie.name} </p>
+                            <span>{movie.year}</span>
+                            <span>{movie.description}</span>
+                            <img src={movie.image} style={{width:'200px'}} alt="movie"/>     
+                    </div>      
+           </div>
+           </div>
+        )
+            }
+      
+ 
